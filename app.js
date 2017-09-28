86c98f943df2e453a690c1ea264944d67a764b60
niwanc <email2niwa@gmail.com>
2017-09-28 11:48:51 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
-/**
- * Created by niwantha on 9/28/17.
- */
-/**
- * Created by niwantha on 9/27/17.
- */
-// var http = require('http');
-//
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end('Hello World!');
-// }).listen(8080);
 
+var restify = require('restify');
+var builder = require('botbuilder');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// middleware error handling
-app.use((err, req, res, next) => {
-    if (res.headersSent) {
-    return next(err);
-}
-console.error(err.stack);
-return res.status(500).json({ message: err.message });
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Example app listening on port 3000!');
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-const newsbot = require('./new');
-newsbot.init(app);
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
