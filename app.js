9cc99ae4036b4cf8b5cc9d119f053bee8bc7e219
niwanc <email2niwa@gmail.com>
2017-09-28 11:52:58 +0530

    initial commit

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 'bd67e9db-adc7-4f76-915f-365b80eaf750',
+    appPassword: 'ZKhv31TZ3X2mpLkftymfXUL'
 });
 
 // Listen for messages from users
