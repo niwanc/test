2dabddb1622438444b957427f6ff7c2624540f5d
niwanc <email2niwa@gmail.com>
2017-09-28 10:03:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by niwantha on 9/28/17.
+ */
+/**
+ * Created by niwantha on 9/27/17.
+ */
+// var http = require('http');
+//
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('Hello World!');
+// }).listen(8080);
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// middleware error handling
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+    return next(err);
+}
+console.error(err.stack);
+return res.status(500).json({ message: err.message });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+const newsbot = require('./new');
+newsbot.init(app);
